5850b237daa408cb33dfa750a10843a9d37acf1c
epolepole <Contagious!7618G!Mythical>
2018-03-23 15:29:23 +0100

    Add views

--- BlogApp/TestingBottle/TestBottle.py
+++ BlogApp/TestingBottle/TestBottle.py
@@ -1,21 +1,33 @@
-from bottle import route, run, template
+from bottle import route, run, template, debug
 from pymongo import MongoClient
 import pymongo
 
 
+@route('/')
+def home_page():
+    mythings = ['apple', 'banana', 'pineapple']
+    return template('hello_world', username="Lope", things=mythings)
+    # return "Hello World\n"
+
+
+@route('/testpage')
+def test_page():
+    return "This is a test page"
+
 
 @route('/read_from_mongo/<collection>')
-def read_mongo(collection):
+def read_mongo(collection='names'):
     connection = MongoClient('localhost', 27017)
     db = connection.test_database
     col = db[collection]
     item = col.find_one()
-    return template('<b>Found this:</b> {{item}}', item=str(item))
+    return template('<b>Hello mister {{item}}</b>', item=str(item['name']))
 
 
 @route('/hello/<name>')
-def index(name):
+def hello(name):
     return template('<b>Helo {{name}}</b>!', name=name)
 
 
+debug(True)
 run(host='localhost', port=8080)
