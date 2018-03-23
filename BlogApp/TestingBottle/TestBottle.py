dcb2b334f1cb4b2e1d0d59327943be54b55929ab
epolepole <Contagious!7618G!Mythical>
2018-03-23 13:34:41 +0100

    Pymongo Test

--- BlogApp/TestingBottle/TestBottle.py
+++ BlogApp/TestingBottle/TestBottle.py
@@ -1,9 +1,21 @@
 from bottle import route, run, template
+from pymongo import MongoClient
+import pymongo
+
+
+
+@route('/read_from_mongo/<collection>')
+def read_mongo(collection):
+    connection = MongoClient('localhost', 27017)
+    db = connection.test_database
+    col = db[collection]
+    item = col.find_one()
+    return template('<b>Found this:</b> {{item}}', item=str(item))
 
 
 @route('/hello/<name>')
 def index(name):
-    return template('<b>Helo {{name}}</b>!', name = name)
+    return template('<b>Helo {{name}}</b>!', name=name)
 
 
 run(host='localhost', port=8080)
